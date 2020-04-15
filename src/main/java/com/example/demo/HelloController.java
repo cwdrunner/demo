3697ace351da2a16fec8f9f08170d7097607c1c1
cwdrunner <nitroxbill@msn.com>
2020-04-15 15:38:41 -0600

    add cr to message

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings Earthling!";
+        return "/nGreetings Earthling!/n";
     }
 
 }
